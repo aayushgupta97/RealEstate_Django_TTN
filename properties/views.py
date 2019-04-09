14116fa8489fe05082f5d47b81a044c54d1d21b5
Aayush Gupta <aayushgupta2097@gmail.com>
2019-04-09 14:06:38 +0530

    pagination on all properties listings page

--- properties/views.py
+++ properties/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse
 from .models import Property
 
@@ -6,9 +7,14 @@ from .models import Property
 
 
 def index(request):
-    listings = Property.objects.all()
+    listings = Property.objects.order_by('-list_date').filter(is_published=True)
+    paginator = Paginator(listings, 6)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
+
     context = {
-        'listings': listings,
+        'listings': paged_listings,
     }
     return render(request, 'properties/properties.html', context)
 
